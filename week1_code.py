2ea8bfb4a2ad0f55ec1ac3658ecf4bb185fe8a8d
Hassan Noun <hassan.noun@outlook.com>
2025-10-12 21:07:18 +1100

    Updated week1_code.py with improved [Fe/H] vs Age plot

--- week1_code.py
+++ week1_code.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Analysing [Fe/H] vs. Age data for Krause dataset
+
+# Obtaining data-set and taking each column separately
 krause = pd.read_csv(r"C:\Users\HassanNoun\OneDrive - UNSW\Desktop\PHYS3116\Group Assessment\GitHubClone_PHYS3116\PHYS3116_Team\Krause21.csv")
 ages = krause["Age"]        # cluster ages
 feh  = krause["FeH"]        # metallicities
@@ -9,8 +12,9 @@ rh   = krause["rh"]         # half-light radius
 c5   = krause["C5"]         # concentration
 names = krause["Object"]    # cluster names
 
-plt.scatter(ages, feh)
-plt.ylabel("[Fe/H]")
-plt.xlabel("Age (Gyr)")
+# Plotting as a scatter plot
+plt.scatter(feh, ages, color='black')
+plt.xlabel("[Fe/H]")
+plt.ylabel("Age (Gyr)")
 plt.title("Age–Metallicity Relation (Krause21)")
 plt.show()
